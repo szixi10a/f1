06583bf8e2d2c2204f4d4b254a912ccf7bf227b3
Várnai Viktor <varnai.viktor>
2023-05-30 12:44:12 +0200

    Slide added

--- slide.js
+++ slide.js
@@ -1,10 +1,10 @@
 const pictures = ["img1.png", "img2.png", "img3.png"]
 let cunt = 0
 function Slide(where){
-    if(where=="balra"){
-
+    if(where=="balra"&&cunt>0){
+        document.getElementById("kep").innerHTML = '<img src="' + pictures[cunt-1] + '" alt="" class="w-100"></img>'
     }
-    else{
-
+    if(where=="jobbra"&&cunt<3){
+        document.getElementById("kep").innerHTML = '<img src="' + pictures[cunt+1] + '" alt="" class="w-100"></img>'
     }
 }
