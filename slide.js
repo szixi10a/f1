572fcb247200820c0bd0191f71b33d055b1c579e
Várnai Viktor <varnai.viktor>
2023-05-30 13:22:02 +0200

    Slide

--- slide.js
+++ slide.js
@@ -1,12 +1,14 @@
 const pictures = ["img1.png", "img2.png", "img3.png"]
+const szoveg = ["Leves", "Főétel", "Sütemény"]
+const url = ["levesek.html", "foetelek.html", "cookie.html"]
 let cunt = 0
 function Slide(where){
     if(where=="balra"&&cunt>0){
-        document.getElementById("kep").innerHTML = '<img src="' + pictures[cunt-1] + '" alt="" class="w-100"></img>'
+        document.getElementById("keps").innerHTML = '<img src="' + pictures[cunt-1] + '" id="kep"><div class="top-left">' + szoveg[cunt-1] + '</div><div class="bottom-right"><a href="' + url[cunt-1] + '">Tovább>></a></div>'
         cunt--
     }
-    if(where=="jobbra"&&cunt<3){
-        document.getElementById("kep").innerHTML = '<img src="' + pictures[cunt+1] + '" alt="" class="w-100"></img>'
+    if(where=="jobbra"&&cunt<=3){
+        document.getElementById("keps").innerHTML = '<img src="' + pictures[cunt+1] + '" id="kep"><div class="top-left">' + szoveg[cunt+1] + '</div><div class="bottom-right"><a href="' + url[cunt+1] + '">Tovább>></a></div>'
         cunt++
     }
 }
